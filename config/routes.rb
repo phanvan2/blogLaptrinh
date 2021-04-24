6dc47eec121089f4f043e931a1777adaa439b5c4
phanvan2 <phanvanphung215@gmail.com>
2021-04-24 08:38:03 +0700

    phungv4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   root 'articles#index'
   get 'articles/index'
+  get 'register', to: 'users#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
