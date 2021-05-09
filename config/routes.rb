11a558535dae3b9f8bd76804695fc94a0f380ef9
kgsonhai <kgsonhai@gmail.com>
2021-05-09 21:57:11 +0700

    hai v2

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   resources :blogs
   get 'footer/index'
   get 'menu/index'
+  get 'admin/index'
   resources :users
   root 'articles#index'
   get 'articles/index'
   get 'register', to: 'users#new'
   get 'menu' , to: 'menu#index'
-  get "user/update", to: 'users#edit'
+  get "user/update", to: 'users#edit' 
   #get 'login', to: 'users#login'
   post 'session' => 'session#create'
   get '/logout' => 'session#destroy'
@@ -22,6 +23,14 @@ Rails.application.routes.draw do
   post '/comments1/create' => 'comments#create'
   post '/reply_comments/create' => 'reply_comments#create'
   post '/update_status' => 'blogs#update_status'
+  # resources :admin
+  get "/admin", to: "admin#index"
+  get "/admin/user", to: "users#index"
+  get "/admin/blog", to: "blogs#index"
+  get "admin/categories", to: "categories#index"
+
+
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
