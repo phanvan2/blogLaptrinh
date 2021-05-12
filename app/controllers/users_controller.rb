300ffe84c937f52b89d7c39e9d2ee6b1106d49ff
phanvan2 <phanvanphung215@gmail.com>
2021-05-12 09:45:46 +0700

    phuung updatee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
         @user = User.new(username: user_params[:username] , email: user_params[:email], password_digest: user_params[:password_digest], quyen: 0, img_user: user_params[:img_user])
         respond_to do |format|
           if @user.save
-            format.html { redirect_to @user, alert: "User was successfully created." }
+            format.html { redirect_to '/login', alert: "User was successfully created." }
             format.json { render :show, status: :created, location: @user }
           else
             format.html { render :new, status: :unprocessable_entity }
